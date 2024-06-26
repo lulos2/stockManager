10fa4f151cbdde95692d3c799df84283b74215c1
Lulo <lulobareiss@gmail.com>
2024-06-26 10:45:51 -0300

    Chore: add slf4j in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,19 @@
-module com.example.stockmanager {
+module app {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.sql;
+    requires org.xerial.sqlitejdbc;
+    requires org.slf4j;
+    requires org.slf4j.simple;
+
+    exports app;
+    exports app.model;
+    exports app.service;
+    exports app.util;
+    exports app.controller to javafx.fxml;
 
     opens app to javafx.fxml;
     opens app.controller to javafx.fxml;
     opens app.model to javafx.base;
-    exports app;
-    exports app.controller to javafx.fxml;
 }
